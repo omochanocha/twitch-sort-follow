e01fb1635378af400181e54e0ca7718079d98236
omochanocha <yokpef@gmail.com>
2026-02-08 23:49:41 +0900

    log仕込み

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,12 @@ import { getToken } from 'next-auth/jwt';
 import { auth } from '@/auth';
 
 export default auth(async function middleware(req: NextRequest) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/strict-boolean-expressions, @typescript-eslint/no-unsafe-member-access
+  console.log('has req.auth?', !!(req as any).auth);
+  console.log('pathname', req.nextUrl.pathname);
+
   const token = await getToken({ req, secret: process.env['NEXTAUTH_SECRET']! });
+  console.log('getToken()', token ? 'HAS_TOKEN' : 'NO_TOKEN');
   const isAuth = !!token;
   const isAuthPage = req.nextUrl.pathname.startsWith('/login');
 

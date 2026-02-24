da38855fe291a4d70addaab63b8bb6c72784bd94
omochanocha <yokpef@gmail.com>
2026-02-24 10:18:39 +0900

    型にreadonlyを付ける修正

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,51 +1,61 @@
 import { z } from 'zod';
 
-export const FollowsSchema = z.object({
-  broadcaster_id: z.string(),
-  broadcaster_login: z.string(),
-  broadcaster_name: z.string(),
-  followed_at: z.iso.datetime(),
-});
+export const FollowsSchema = z
+  .object({
+    broadcaster_id: z.string(),
+    broadcaster_login: z.string(),
+    broadcaster_name: z.string(),
+    followed_at: z.iso.datetime(),
+  })
+  .readonly();
 
 export type Follows = z.infer<typeof FollowsSchema>;
 
-export const FollowsResponseSchema = z.object({
-  data: FollowsSchema.array(),
-  pagination: z.object({
-    cursor: z.string().optional(),
-  }),
-  total: z.number(),
-});
+export const FollowsResponseSchema = z
+  .object({
+    data: FollowsSchema.array(),
+    pagination: z.object({
+      cursor: z.string().optional(),
+    }),
+    total: z.number(),
+  })
+  .readonly();
 
 export type FollowsResponse = z.infer<typeof FollowsResponseSchema>;
 
-export const UserSchema = z.object({
-  broadcaster_type: z.string(),
-  created_at: z.iso.datetime(),
-  description: z.string(),
-  display_name: z.string(),
-  id: z.string(),
-  login: z.string(),
-  offline_image_url: z.string(),
-  profile_image_url: z.string(),
-  type: z.string(),
-  view_count: z.number(),
-});
+export const UserSchema = z
+  .object({
+    broadcaster_type: z.string(),
+    created_at: z.iso.datetime(),
+    description: z.string(),
+    display_name: z.string(),
+    id: z.string(),
+    login: z.string(),
+    offline_image_url: z.string(),
+    profile_image_url: z.string(),
+    type: z.string(),
+    view_count: z.number(),
+  })
+  .readonly();
 
 export type User = z.infer<typeof UserSchema>;
 
-export const UsersResponseSchema = z.object({
-  data: UserSchema.array(),
-});
+export const UsersResponseSchema = z
+  .object({
+    data: UserSchema.array(),
+  })
+  .readonly();
 
-export const TwitchResponseSchema = z.object({
-  id: z.string(),
-  display_name: z.string(),
-  login: z.string(),
-  profile_image_url: z.string(),
-  offline_image_url: z.string(),
-  followed_at: z.string(),
-});
+export const TwitchResponseSchema = z
+  .object({
+    id: z.string(),
+    display_name: z.string(),
+    login: z.string(),
+    profile_image_url: z.string(),
+    offline_image_url: z.string(),
+    followed_at: z.string(),
+  })
+  .readonly();
 
 export const TwitchResponseListSchema = TwitchResponseSchema.array();
 
